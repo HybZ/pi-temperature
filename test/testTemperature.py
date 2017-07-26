1c0ec25dbdd7f17aee5f8c23723606199e551ebe
Adi Bajramov <adi.bajramov@gmail.com>
2017-07-27 00:50:01 +0200

    Testing the Flask application

---
+++ test/testTemperature.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+import sys
+sys.path.append('../src')
+from controller.TemperatureController import temperatureBlueprint
+import unittest
+
+class TemperatureTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.register_blueprint(temperatureBlueprint)
+        self.app = self.app.test_client()
+
+    def test_empty_db(self):
+        rv = self.app.get('/temperature')
+        assert b'Temperature : Data not good, skip' in rv.data
+        assert b'Humidity : Data not good, skip' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
